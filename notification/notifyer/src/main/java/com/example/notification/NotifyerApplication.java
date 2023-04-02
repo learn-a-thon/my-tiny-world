5be533ccdbbc8a0c5849be9896aaf2d5c58cb4c6
JaeYeong Lee <dldustn14@gmail.com>
2023-04-02 21:37:34 +0900

    [jylee] 프로젝트 생성 및 요구사항 선정 (#5)
    * init: 프로젝트 초기 설정
    
    * chore: 모듈 분리

---
+++ notification/notifyer/src/main/java/com/example/notification/NotifyerApplication.java
@@ -0,0 +1,11 @@
+package com.example.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NotifyerApplication.class, args);
+    }
+}
